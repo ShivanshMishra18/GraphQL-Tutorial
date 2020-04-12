6d52e24a265a4590170e4f1d49da9ed86614703b
ShivanshMishra18 <shivansh18@iitg.ac.in>
2020-04-12 10:56:40 +0530

    Custom types and funtion args

---
+++ src/3.DataTypes2.js
@@ -0,0 +1,87 @@
+import { GraphQLServer } from 'graphql-yoga'
+
+// Custom types - User defined (complex) data types
+//      Easy can be easily noted that primitive types are 
+//      often not useful in real world applications
+
+
+// Note here that we name our types and use them to 
+// specify the type in Query. ! means the same 
+// We will look at setting up relations in some time
+
+const typeDefs = `
+
+    type Query {
+        doctor: Doctor!
+        hospital: Hospital!
+        patient: Patient!
+    }
+
+    type Doctor {
+        id: ID!
+        name: String!
+        age: Int
+        specialization: String!
+    }
+
+    type Hospital {
+        id: ID!
+        name: String!
+        rating: Float
+    }
+
+    type Patient {
+        id: ID!
+        name: String!
+        problem: String!
+        age: Int!
+    }
+
+    `
+
+const resolvers = {
+    Query: {
+        doctor() {
+            return {
+                id: '173fks3',
+                name: 'AB Nath',
+                specialization: 'Neurologist'
+            }
+        },
+        hospital() {
+            return {
+                id: 'hsp1190',
+                name: 'Neurokare Hospital',
+                rating: 3.4
+            }
+        },
+        patient() {
+            return {
+                id: 'nrv6202',
+                name: 'Nabin Nath',
+                problem: 'Neuro - UV Syndrome',
+                age: 33
+            }
+        }
+    }
+} 
+
+const GServer = new GraphQLServer({ typeDefs, resolvers })
+GServer.start(() => {
+    console.log("Server is running on port 4000")
+})
+
+
+/*  sample query
+query {
+	hospital {
+    id
+    name
+  }
+  patient {
+    name
+    age
+    problem
+  }
+}
+*/
---
+++ src/4.DataTypes3.js
@@ -0,0 +1,88 @@
+import { GraphQLServer } from 'graphql-yoga'
+
+// Passing arguments to resolvers
+//      Resolvers functions have 4 parameters passed to them.
+//      1.parent 2.args 3.ctx(i.e.context) 4.info
+//      The ones which we want to use are available in the args
+//      parameter as json objects. I will describe the others later.
+
+// This example is continued from previous part
+
+// Again for compulsory argument use !
+
+const typeDefs = `
+
+    type Query {
+        welcome(name: String, id:ID!): String!
+        doctor: Doctor!
+        hospital: Hospital!
+        patient: Patient!
+    }
+
+    type Doctor {
+        id: ID!
+        name: String!
+        age: Int
+        specialization: String!
+    }
+
+    type Hospital {
+        id: ID!
+        name: String!
+        rating: Float
+    }
+
+    type Patient {
+        id: ID!
+        name: String!
+        problem: String!
+        age: Int!
+    }
+
+    `
+
+const resolvers = {
+    Query: {
+        welcome(parent,args,ctx,info) {
+            var name = args.name
+            const id = args.id
+            if (!name)
+                name = "User"
+            return 'Welcome '+name+'. Your id is '+id
+        },
+        doctor() {
+            return {
+                id: '173fks3',
+                name: 'AB Nath',
+                specialization: 'Neurologist'
+            }
+        },
+        hospital() {
+            return {
+                id: 'hsp1190',
+                name: 'Neurokare Hospital',
+                rating: 3.4
+            }
+        },
+        patient() {
+            return {
+                id: 'nrv6202',
+                name: 'Nabin Nath',
+                problem: 'Neuro - UV Syndrome',
+                age: 33
+            }
+        }
+    }
+} 
+
+const GServer = new GraphQLServer({ typeDefs, resolvers })
+GServer.start(() => {
+    console.log("Server is running on port 4000")
+})
+
+
+/*  sample query -- note that name is optional
+query {
+  welcome(id: "abd145od")
+}
+*/
